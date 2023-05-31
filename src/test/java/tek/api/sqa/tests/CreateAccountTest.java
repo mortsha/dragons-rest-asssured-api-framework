da888cf52713d8afd679c943cbfb8df5e2483a8b
Morteza <morteza.sharifi.1995@gmail.com>
2023-05-30 21:25:47 -0400

    new changes in pom file

---
+++ src/test/java/tek/api/sqa/tests/CreateAccountTest.java
@@ -0,0 +1,52 @@
+package tek.api.sqa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import tek.api.model.PrimaryAccount;
+import tek.api.sqa.base.APITestConfig;
+import tek.api.utility.DataGenerator;
+import tek.api.utility.EndPoints;
+
+public class CreateAccountTest extends APITestConfig {
+
+	private DataGenerator data = new DataGenerator();
+	
+	@Test
+	public void createAccountValidTest() {
+
+		String token = getValidToken();
+
+		RequestSpecification request = RestAssured.given();
+		request.header("Authorization", "Bearer " + token);
+		request.contentType(ContentType.JSON);
+
+		// create request body and then add to request
+		PrimaryAccount requestBody = new PrimaryAccount();
+		String firstName = data.getFirstName();
+		String lastName = data.getLastName();
+		requestBody.setEmail(data.getEmail(firstName , lastName , "Canada.com"));
+		requestBody.setFirstName(firstName);
+		requestBody.setLastName(lastName);
+		requestBody.setTitle("Mr.");
+		requestBody.setGender("MALE");
+		requestBody.setMaritalStatus("SINGLE");
+		requestBody.setEmploymentStatus(data.getJobTitle());
+		requestBody.setDateOfBirth(data.getDateOfBirth());
+
+		request.body(requestBody);
+
+		Response response = request.when().post(EndPoints.CREATE_NEW_ACCOUNT.getValue());
+		response.prettyPrint();
+		String actualEmail = response.jsonPath().getString("email");
+		Assert.assertEquals(response.getStatusCode(), 201);
+
+//		PrimaryAccount responseBody = response.as(PrimaryAccount.class);
+		Assert.assertEquals(actualEmail, requestBody.getEmail());
+	}
+
+}
